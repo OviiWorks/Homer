4add628d0d1812df38dd04c71f4204e4b403a10c
Ovii <ovii@oviiworks.com>
2020-10-04 02:22:27 +0300

    Fragments added

--- app/src/main/java/com/example/homer/MainActivity.kt
+++ app/src/main/java/com/example/homer/MainActivity.kt
@@ -1,11 +1,66 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.homer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.homer.fragments.ChatFragment
+import com.example.homer.fragments.SearchFragment
+import com.example.homer.fragments.SettingsFragment
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addFragment(ChatFragment(),   "Chats")
+        viewPagerAdapter.addFragment(SearchFragment(),  "Search")
+        viewPagerAdapter.addFragment(SettingsFragment(), "Settings")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+    internal class ViewPagerAdapter(fragmentManager:FragmentManager) :
+            FragmentPagerAdapter(fragmentManager)
+    {
+        private val fragments : ArrayList<Fragment>
+        private val titles : ArrayList<String>
+
+        init {
+            fragments = ArrayList<Fragment>()
+            titles = ArrayList<String>()
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String)
+        {
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(i: Int): CharSequence? {
+            return titles[i]
+        }
     }
 }
